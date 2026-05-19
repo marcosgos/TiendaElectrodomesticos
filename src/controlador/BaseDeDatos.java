f703a8df56479dca48563a70f8e889623444421b
Marcos <marcutigon@gmail.com>
2026-05-19 15:54:11 +0200

    Commit general arreglando la version del JDK

---
+++ src/controlador/BaseDeDatos.java
@@ -0,0 +1,242 @@
+package controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import modelo.Electrodomestico;
+import vista.Consola;
+
+public class BaseDeDatos {
+	
+	/**
+	 * Método para la creación de la conexión a la base de datos.
+	 * @return
+	 * @throws SQLException
+	 */
+	public Connection getConexion() throws SQLException {
+		String url = "jdbc:mysql://localhost/bd_electrodomesticos";
+	    //String url = "jdbc:mysql://192.168.1.3:3306/bd_electrodomesticos?useSSL=false";
+	    String usuario = "root";
+	    //String usuario = "marcos";
+		String constraseña = "";
+	    //String constraseña = "123456";
+
+	    return DriverManager.getConnection(url, usuario, constraseña);
+	}
+	
+	/**
+	 * Método por el que insertamos un electrodomestico en la base de datos
+	 * @return del objeto electrodomestico insertado para guardar en un arrayList
+	 */
+	public Electrodomestico insertarElectrodomestico() {
+		Consola c = new Consola();
+		Electrodomestico e = new Electrodomestico();
+		e = c.pideElectrodomestico();
+		Connection conexion;
+				
+		try {
+			conexion = getConexion();
+			Statement consulta = conexion.createStatement();
+			
+			consulta.executeUpdate("insert into electrodomestico (tipo,marca,precio,consumo,vendido) values('"
+					+e.getTipo()+"','"+e.getMarca()+"',"+e.getPrecio()+","+e.getConsumo()+","+e.isVendido()+")");
+			conexion.close();
+			System.out.println("Producto guardado correctamente");
+		} catch (SQLException ex) {
+			System.out.println("No se pudo insertar el electrodomestico");
+			
+		}
+		return e;
+		
+	}
+	
+	/**
+	 * Método por el que se consulta un electrodomestico de la base de datos
+	 */
+	public void consultaElectrodomesticos() {
+		Consola c = new Consola();
+		Connection conexion;	
+		
+		try {
+			conexion = getConexion();
+			Statement consulta = conexion.createStatement();
+			ResultSet listado = consulta.executeQuery("select id_electrodomestico, tipo "
+														+ "from electrodomestico");
+			while(listado.next()) {
+				System.out.println("Identificacion: " + listado.getString("id_electrodomestico"));
+				System.out.println("Tipo: " + listado.getString("tipo"));
+			}
+			
+			int id_electrodomestico = c.pideEntero("Elija un identificador de electrodomestico: ");
+			ResultSet registro = consulta.executeQuery("select * "
+														+ "from electrodomestico "
+														+ "where id_electrodomestico ="
+														+ id_electrodomestico);
+			if(registro.next()) {
+				System.out.println("Id_producto: " + registro.getString("id_electrodomestico"));
+				System.out.println("Tipo: " + registro.getString("tipo"));
+				System.out.println("Marca: " + registro.getString("marca"));
+				System.out.println("Precio: " + registro.getString("precio"));
+				System.out.println("Consumo: " + registro.getString("consumo"));
+			} else {
+				System.out.println("El electrodomestico con el ID "+ id_electrodomestico + " no existe");
+			}
+			
+			conexion.close();					
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Método en el que se modifica un producto de la base de datos.
+	 */
+	public void modificarElectrodomestico() {
+		Connection conexion;
+		Consola c = new Consola();
+		
+		try {
+			conexion = getConexion();
+			
+			Statement consulta = conexion.createStatement();
+			ResultSet registro = consulta.executeQuery("select id_Electrodomestico, tipo from electrodomestico ");
+			
+			System.out.println("\nID -- Tipo");
+			while(registro.next()) {
+				System.out.println(registro.getString("id_Electrodomestico")+"  -- "+ registro.getString("tipo"));
+			}
+			conexion.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		int idElectrodomestico = c.pideEntero("Introduzca el ID del electrodomestico a modificar: ");
+		Electrodomestico e = c.pideElectrodomestico();
+		
+		try {
+			conexion = getConexion();
+			Statement consulta = conexion.createStatement();
+			int valor = consulta.executeUpdate("UPDATE electrodomestico SET tipo='" + e.getTipo() + "', marca='" + e.getMarca() + "', precio=" + e.getPrecio() + ", consumo=" + e.getConsumo() + " WHERE id_electrodomestico=" + idElectrodomestico);
+			
+			if(valor==1) {
+				System.out.println("\t\nArtículo modificado correctamente\n");
+			}else {
+				System.out.println("\t\nNo existe un Electrodomestico con dicho identificador\n");
+			}
+			conexion.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Método en el que se borra un producto de la base de datos.
+	 */
+	public void borrarElectrodomestico() {
+		Connection conexion;
+		Consola c = new Consola();
+		
+		try {
+			conexion = getConexion();
+			
+			Statement consulta = conexion.createStatement();
+			ResultSet registro = consulta.executeQuery("select id_Electrodomestico, tipo from electrodomestico ");
+			
+			System.out.println("\nID -- Tipo");
+			while(registro.next()) {
+				System.out.println(registro.getString("id_Electrodomestico")+"  -- "+ registro.getString("tipo"));
+			}
+			conexion.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		int idElectrodomestico = c.pideEntero("Introduzca el ID del electrodomestico a borrar: ");
+		
+		try {
+			conexion = getConexion();
+			Statement consulta = conexion.createStatement();
+			System.out.println("¿Está seguro de que quiere eliminarlo?");
+		    System.out.println("1. Sí, estoy seguro.");
+		    System.out.println("2. No");
+		    int opcion = c.pideEntero("Elija una opción: ");
+
+		    if(opcion == 1) {
+		        int valor = consulta.executeUpdate("DELETE FROM electrodomestico WHERE id_electrodomestico=" + idElectrodomestico);
+		        if(valor == 1) {
+		            System.out.println("\nArtículo eliminado correctamente\n");
+		        } else {
+		            System.out.println("\nNo existe un Electrodoméstico con dicho identificador\n");
+		        }
+		    } else {
+		        System.out.println("\nOperación cancelada\n");
+		    }
+		    conexion.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Método para consultar a la base de datos los electrodomésticos que consuman menos del 15%
+	 * @return
+	 */
+	public ArrayList<Electrodomestico> consultarMenor15() {
+		ArrayList<Electrodomestico> filtro = new ArrayList<>();
+		Connection conexion;
+		
+		try {
+			conexion = getConexion();
+			Statement consulta = conexion.createStatement();
+			ResultSet electrodomesticosMenoresA15;
+			
+			electrodomesticosMenoresA15 = consulta.executeQuery("select id_electrodomestico, tipo, marca, consumo from electrodomestico where consumo <= 15");
+			
+			if(!electrodomesticosMenoresA15.next()) {
+				return null;
+			}else {
+				do {
+					Electrodomestico electrodomesticoNuevo = new Electrodomestico();
+					electrodomesticoNuevo.setId(electrodomesticosMenoresA15.getInt("id_electrodomestico"));
+					electrodomesticoNuevo.setTipo(electrodomesticosMenoresA15.getString("tipo"));
+					electrodomesticoNuevo.setMarca(electrodomesticosMenoresA15.getString("marca"));
+					electrodomesticoNuevo.setConsumo(electrodomesticosMenoresA15.getFloat("consumo"));
+					filtro.add(electrodomesticoNuevo);
+				}while(electrodomesticosMenoresA15.next());
+				return filtro;
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	
+	/**
+	 * Metodo para consultar a la base de datos la media del precio de los electrodomésticos
+	 */
+	public void mostrarMediaPrecios() { 
+	    	BaseDeDatos basededatos = new BaseDeDatos();
+	    	Connection conexion;
+	        try {
+	            conexion = basededatos.getConexion();
+	            String sql = "SELECT AVG(precio) AS Media_Precios FROM electrodomestico";
+	            Statement frase = conexion.createStatement();
+	            ResultSet res = frase.executeQuery(sql);
+
+	            if (res.next()) {
+	                double media = res.getDouble("Media_Precios");
+	                System.out.println("La media de precios es: " + media);
+	            }
+
+	            res.close();
+	            frase.close();
+	            conexion.close();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	}
+}
---
+++ src/vista/Consola.java
@@ -0,0 +1,124 @@
+package vista;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import controlador.BaseDeDatos;
+import modelo.Electrodomestico;
+
+public class Consola {
+	
+	/**
+	 * Método para pedir al usuario un texto
+	 * @param mensaje (le pasamos la pregunta)
+	 * @return del texto que ha introducido el usuario
+	 */
+	public String pideCadena(String mensaje) {
+		Scanner leer = new Scanner(System.in);
+        System.out.print(mensaje);
+        return leer.nextLine();
+    }
+	
+	/**
+	 * Método para pedir al usuario un numero entero
+	 * @param mensaje (le pasamos la pregunta)
+	 * @return del número introducido por el usuario
+	 */
+    public int pideEntero(String mensaje) {
+    	Scanner leer = new Scanner(System.in);
+        int numero = 0;
+        boolean letra;
+        do {
+            System.out.print(mensaje);
+        	letra = true;
+	        try{
+		        numero = leer.nextInt();
+		        leer.nextLine();
+		        letra = false;
+	        }catch (InputMismatchException e) {
+				System.out.println("\n\t¡Has introducido un carácter!\n");
+				leer.next();
+			}
+        }while(letra);
+        return numero;
+    }
+    
+    /**
+     * Método para pedir verdadero o falso al usuario
+     * @param mensaje (le pasamos la pregunta)
+     * @return del true o false introducido 
+     */
+    public boolean pideBooleano(String mensaje) {
+    	Scanner leer = new Scanner(System.in);
+        System.out.print(mensaje + " (true/false): ");
+        boolean valor = leer.nextBoolean();
+        leer.nextLine();
+        return valor;
+    }
+    
+    /**
+     * Menú principal de la aplicación, se repetirá hasta que el usuario seleccione una opción registrada.
+     * @return el número asociado a la opción deseada por el usuario.
+     */
+    public int menuPrincipal() {
+    	int opcion;
+    	do {
+	    	System.out.println("\n\tMenu Electrodomestico\n");
+	    	System.out.println("Opción 1. Crear un electrodoméstico");
+	    	System.out.println("Opción 2. Modificar un electrodoéstico");
+	    	System.out.println("Opción 3. Borrar un electrodoméstico");
+	    	System.out.println("Opción 4. Consultar un electrodoméstico");
+	    	System.out.println("Opción 5. Consultar la media de los precios");
+	    	System.out.println("Opción 6. Consultar los electrodomésticos con menos de 15% de consumo");
+	    	System.out.println("Opción 0. Salir ");
+	    	opcion = pideEntero("Voy a elegir la opción: ");
+	    	if(opcion<0 || opcion>6) {
+	    		System.out.println("\n\tOpción fuera de rango");
+	    	}
+    	}while(opcion<0 || opcion>6);
+
+		return opcion;
+    }
+    
+    /**
+     * Método parqa pedir objeto electrodoméstico al usuario con todas sus especificaciones
+     * @return del objeto relleno de información por el usuario
+     */
+    public Electrodomestico pideElectrodomestico(){
+    	Electrodomestico e = new Electrodomestico();
+        
+        e.setTipo(pideCadena("Introduce el tipo de electrodomestico: "));
+        e.setMarca(pideCadena("Introduce la marca del electrodomestico: "));
+        e.setPrecio(pideEntero("Introduce el precio del electrodomestico: "));
+        e.setConsumo(pideEntero("Introduce el consumo del electrodomestico: "));
+        e.setVendido(pideBooleano("¿Está vendido el electrodoméstico?: "));
+        
+        
+        return e;
+
+    }
+    
+    /**
+     * Método para consultar electrodoméstico que consuma menos del 15%
+     */
+    public void consultarMenor15() {
+
+        BaseDeDatos bd = new BaseDeDatos();
+
+        ArrayList<Electrodomestico> lista = bd.consultarMenor15();
+
+        if(lista != null) {
+            for(Electrodomestico e : lista) {
+                System.out.println();
+                System.out.println("Identificador: " + e.getId());
+                System.out.println("Tipo: " + e.getTipo());
+                System.out.println("Marca: " + e.getMarca());
+                System.out.println("Consumo: " + e.getConsumo());
+            }
+        } else {
+            System.out.println("\n\tNo hay ningún electrodoméstico con esas características\n");
+        }
+    }
+    
+}
